045cc5b7f9b6e860f32b946834244dc41a6480e2
Maxime Griot <yamashi@live.com>
2019-08-16 17:03:39 +0200

    D3D11 hooking

--- Code/hooks/include/D3D11Hook.h
+++ Code/hooks/include/D3D11Hook.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <Signal.h>
+
+struct IDXGISwapChain;
 struct D3D11Hook
 {
     D3D11Hook(D3D11Hook&&) = delete;
     D3D11Hook(const D3D11Hook&) = delete;
     D3D11Hook& operator=(D3D11Hook&&) = delete;
     D3D11Hook& operator=(const D3D11Hook&) = delete;
+
+    Signal<void(IDXGISwapChain*)> OnCreate;
+    Signal<void(IDXGISwapChain*)> OnPresent;
+    Signal<void(IDXGISwapChain*)> OnLost;
 
     static void Install();
     static D3D11Hook& Get();

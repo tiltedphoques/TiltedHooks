343854dc3e3e7a194ea19e34acbb49083ada95ad
yamashi <yamashi@live.com>
2021-02-23 19:27:21 +0100

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,8 +2,7 @@ set_languages("cxx17")
 
 set_xmakever("2.5.1")
 
-add_requires("tiltedcore", "mimalloc", "hopscotch-map")
-add_requires("catch2")
+add_requires("tiltedcore", "hopscotch-map", "catch2")
 
 add_rules("mode.debug","mode.releasedbg", "mode.release")
 add_rules("plugin.vsxmake.autoupdate")
@@ -19,7 +18,7 @@ target("TiltedHooks")
     set_kind("static")
     set_group("Libraries")
     add_files("Code/hooks/src/*.cpp")
-    add_includedirs("Code/hooks/include/")
+    add_includedirs("Code/hooks/include/", {public = true})
     add_headerfiles("Code/hooks/include/*.hpp", {prefixdir = "TiltedHooks"})
     add_syslinks("dxguid", "dinput8", "d3d11")
     add_packages("tiltedcore", "mimalloc", "hopscotch-map")
